d763459d29e8dc679a1fe2fcc9e109e13b56ef29
swapnil-shinde <swapnilshinde7900@gmail.com>
2023-09-25 17:36:30 +0530

    Video 8: Added logic to count words

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -3,10 +3,15 @@ import { useState } from 'react'
 
 export default function Textarea() {
 
-  const [text, setText] = useState('Enter text here to convert in Upper Case... ')
+  const [text, setText] = useState('')
   
-  const handleClick = () => {
+  const handleUpClick = () => {
     let newText = text.toUpperCase()
+    setText(newText)
+  }
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase()
     setText(newText)
   }
 
@@ -19,7 +24,15 @@ export default function Textarea() {
       <div className="mb-3 container">
         <h1>Enter text here...</h1>
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-        <button className='btn btn-primary' onClick={handleClick}>Convert to Upper Case</button>
+        <button className='btn btn-primary mt-3' onClick={handleUpClick}>Convert to Upper Case</button>
+        <button className='btn btn-primary mt-3 mx-3' onClick={handleLoClick}>Convert to Lower Case</button>
+      </div>
+      <div className='container'>  
+        <h2>Your text summary</h2>
+        <p>{text == '' ? 0 : text.split(' ').length} words and {text.length} characters</p>
+        <p>{text == '' ? 0 : 0.008 * text.split(' ').length} Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
       </div>
     </div>
   )
